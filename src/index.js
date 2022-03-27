659cb05c6e9c7516c5b0bc6ec10876d2a3e5065f
iwobrzyski <i.brzyski@commonwombat.pl>
2022-03-27 22:02:37 +0200

    Initialize basic redux for the project.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from "react-router-dom";
+import {BrowserRouter} from "react-router-dom";
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createStore} from 'redux';
+import rootReducer from "./redux/reducers";
+import {Provider} from "react-redux";
+
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App/>
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
